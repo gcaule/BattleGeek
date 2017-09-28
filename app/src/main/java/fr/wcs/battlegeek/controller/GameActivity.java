17c8fe8992d50cf32196dc1055432a787675089d
Adphi <philippe.adrien.nousse@gmail.com>
2017-09-28 15:56:37 +0200

    Add Victory and Drown

--- app/src/main/java/fr/wcs/battlegeek/controller/GameActivity.java
+++ app/src/main/java/fr/wcs/battlegeek/controller/GameActivity.java
@@ -1,5 +1,6 @@
 package fr.wcs.battlegeek.controller;
 
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.os.Build;
 import android.os.Bundle;
@@ -15,8 +16,7 @@ import fr.wcs.battlegeek.ui.GameView;
 public class GameActivity extends AppCompatActivity {
 
     private String TAG = "CustomView";
-    private Toast toast;
-    
+
     private char[][] mMap = Maps.getMap();
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -26,6 +26,8 @@ public class GameActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_game);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
+
+        final Context mContext = getApplicationContext();
         
         final GameView gameView = (GameView) findViewById(R.id.gameView);
         gameView.setOnPlayListener(new GameView.PlayListener() {
@@ -36,20 +38,42 @@ public class GameActivity extends AppCompatActivity {
                     gameView.setPlouf(x, y);
                     mMap[y][x] = '_';
                 } else if (Character.isLowerCase(result) || result == '_') {
-                    Toast.makeText(getApplicationContext(), "Heu... Ca sert à rien ça ...", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(mContext, getString(R.string.alreadyPlayedMessage), Toast.LENGTH_SHORT).show();
                 } else {
                     gameView.setTouch(x, y, result);
                     mMap[y][x] = Character.toLowerCase(result);
+                    if(isDrown(result)) {
+                        Toast.makeText(mContext, getString(R.string.itemDrownMessage), Toast.LENGTH_SHORT).show();
+                    }
+                    if(victory()) {
+                        Toast.makeText(mContext, getString(R.string.victoryMessage), Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
 
         });
     }
 
-
+    private boolean isDrown(char symbol) {
+        for(char[] row : mMap){
+            for(char letter : row){
+                if(symbol == letter) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
     private boolean victory() {
-        return false;
+        for(char[] row : mMap) {
+            for(char symbol: row) {
+                if(Character.isUpperCase(symbol)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 }
