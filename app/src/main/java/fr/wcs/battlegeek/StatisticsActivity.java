ce9d09c818d4c545c07bf058d8c7b7842dfed553
Adphi <philippe.adrien.nousse@gmail.com>
2017-10-14 12:10:03 +0200

    Add Spinner to Statistics and full stats implementation

--- app/src/main/java/fr/wcs/battlegeek/StatisticsActivity.java
+++ app/src/main/java/fr/wcs/battlegeek/StatisticsActivity.java
@@ -3,13 +3,23 @@ package fr.wcs.battlegeek;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import fr.wcs.battlegeek.controller.AI;
 import fr.wcs.battlegeek.controller.DataController;
 import fr.wcs.battlegeek.model.PlayerModel;
 
-public class StatisticsActivity extends AppCompatActivity {
+import static fr.wcs.battlegeek.R.id.textViewLevelGames1;
+import static fr.wcs.battlegeek.R.id.textViewLevelRatio1;
+
+public class StatisticsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private final String TAG = "Statistics";
 
     private PlayerModel mPlayer;
@@ -19,80 +29,68 @@ public class StatisticsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
 
+        final Spinner spinner = (Spinner) findViewById(R.id.spinnerLevel);
+        ArrayList<String> spinnerLevels = new ArrayList<>();
+        spinnerLevels.add(getString(R.string.button_easy));
+        spinnerLevels.add(getString(R.string.button_medium));
+        spinnerLevels.add(getString(R.string.button_hard));
+        spinnerLevels.add(getString(R.string.button_impossible));
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerLevels);
+        spinner.setAdapter(spinnerAdapter);
+        spinner.setOnItemSelectedListener(StatisticsActivity.this);
+
         DataController dataController = new DataController(getApplicationContext());
         dataController.setDataReadyListener(new DataController.DataReadyListener() {
             @Override
             public void onDataReadyListener(PlayerModel player) {
                 mPlayer = player;
-                initView();
+                spinner.setSelection(0);
             }
         });
 
+        ImageButton buttonHome = (ImageButton) findViewById(R.id.imageButtonHome);
+        buttonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 
-    void initView() {
-        Log.d(TAG, mPlayer.toString());
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Log.d(TAG, "onItemSelected() called with: adapterView = [" + adapterView + "], view = [" + view + "], i = [" + i + "], l = [" + l + "]");
+        AI.Level[] levels = new AI.Level[] {AI.Level.I, AI.Level.II, AI.Level.III, AI.Level.IMPOSSIBLE};
 
         // Game Parts
-        TextView textViewLevelGames1 = (TextView) findViewById(R.id.textViewLevelGames1);
-        textViewLevelGames1.setText(mPlayer.getGameParts().get(String.valueOf(AI.Level.I)).toString());
-        /*TextView textViewLevelGames2 = (TextView) findViewById(R.id.textViewLevelGames2);
-        textViewLevelGames2.setText(mPlayer.getGameParts().get(String.valueOf(AI.Level.II)).toString());
-        TextView textViewLevelGames3 = (TextView) findViewById(R.id.textViewLevelGames3);
-        textViewLevelGames3.setText(mPlayer.getGameParts().get(String.valueOf(AI.Level.III)).toString());
-        TextView textViewLevelGames4 = (TextView) findViewById(R.id.textViewLevelGames4);
-        textViewLevelGames4.setText(mPlayer.getGameParts().get(String.valueOf(AI.Level.IMPOSSIBLE)).toString());*/
+        TextView textViewLevelGames = (TextView) findViewById(textViewLevelGames1);
+        textViewLevelGames.setText(mPlayer.getGameParts().get(String.valueOf(levels[i])).toString());
 
         // Victory / Defeats
-        TextView textViewLevelVictoryDefeates1 = (TextView) findViewById(R.id.textViewLevelVictoryDefeats1);
-        textViewLevelVictoryDefeates1.setText(mPlayer.getVictories().get(String.valueOf(AI.Level.I)).toString()
-        + "/" + mPlayer.getDefeats().get(String.valueOf(AI.Level.I)).toString());
-        /*TextView textViewLevelVictoryDefeates2 = (TextView) findViewById(R.id.textViewLevelVictoryDefeats2);
-        textViewLevelVictoryDefeates2.setText(mPlayer.getVictories().get(String.valueOf(AI.Level.II)).toString()
-                + "/" + mPlayer.getDefeats().get(String.valueOf(AI.Level.II)).toString());
-        TextView textViewLevelVictoryDefeates3 = (TextView) findViewById(R.id.textViewLevelVictoryDefeats3);
-        textViewLevelVictoryDefeates3.setText(mPlayer.getVictories().get(String.valueOf(AI.Level.III)).toString()
-                + "/" + mPlayer.getDefeats().get(String.valueOf(AI.Level.III)).toString());
-        TextView textViewLevelVictoryDefeates4 = (TextView) findViewById(R.id.textViewLevelVictoryDefeats4);
-        textViewLevelVictoryDefeates4.setText(mPlayer.getVictories().get(String.valueOf(AI.Level.IMPOSSIBLE)).toString()
-                + "/" + mPlayer.getDefeats().get(String.valueOf(AI.Level.IMPOSSIBLE)).toString());*/
+        TextView textViewLevelVictoryDefeates = (TextView) findViewById(R.id.textViewLevelVictoryDefeats1);
+        textViewLevelVictoryDefeates.setText(mPlayer.getVictories().get(String.valueOf(levels[i])).toString()
+                + "/" + mPlayer.getDefeats().get(String.valueOf(levels[i])).toString());
 
         // Ratio
-        TextView textViewLevelRatio1 = (TextView) findViewById(R.id.textViewLevelRatio1);
-        textViewLevelRatio1.setText(mPlayer.getRatio().get(String.valueOf(AI.Level.I)).toString());
-        /*TextView textViewLevelRatio2 = (TextView) findViewById(R.id.textViewLevelRatio2);
-        textViewLevelRatio2.setText(mPlayer.getRatio().get(String.valueOf(AI.Level.II)).toString());
-        TextView textViewLevelRatio3 = (TextView) findViewById(R.id.textViewLevelRatio3);
-        textViewLevelRatio3.setText(mPlayer.getRatio().get(String.valueOf(AI.Level.III)).toString());
-        TextView textViewLevelRatio4 = (TextView) findViewById(R.id.textViewLevelRatio4);
-        textViewLevelRatio4.setText(mPlayer.getRatio().get(String.valueOf(AI.Level.IMPOSSIBLE)).toString());*/
+        TextView textViewLevelRatio = (TextView) findViewById(textViewLevelRatio1);
+        textViewLevelRatio.setText(mPlayer.getRatio().get(String.valueOf(levels[i])).toString());
 
         // Best Time
-        TextView textViewLevelBestTime1 = (TextView) findViewById(R.id.textViewLevelBestTime1);
-        String time1 = mPlayer.getBestTime().get(String.valueOf(AI.Level.I)).toString();
-        textViewLevelBestTime1.setText(time1.equals("2147483647") ? "-" : time1);
-        /*TextView textViewLevelBestTime2 = (TextView) findViewById(R.id.textViewLevelBestTime2);
-        String time2 = mPlayer.getBestTime().get(String.valueOf(AI.Level.II)).toString();
-        textViewLevelBestTime2.setText(time1.equals("2147483647") ? "-" : time2);
-        TextView textViewLevelBestTime3 = (TextView) findViewById(R.id.textViewLevelBestTime3);
-        String time3 = mPlayer.getBestTime().get(String.valueOf(AI.Level.III)).toString();
-        textViewLevelBestTime3.setText(time1.equals("2147483647") ? "-" : time3);
-        TextView textViewLevelBestTime4 = (TextView) findViewById(R.id.textViewLevelBestTime4);
-        String time4 = mPlayer.getBestTime().get(String.valueOf(AI.Level.IMPOSSIBLE)).toString();
-        textViewLevelBestTime4.setText(time1.equals("2147483647") ? "-" : time4);*/
+        TextView textViewLevelBestTime = (TextView) findViewById(R.id.textViewLevelBestTime1);
+        String time1 = mPlayer.getBestTime().get(String.valueOf(levels[i])).toString();
+        textViewLevelBestTime.setText(time1.equals("2147483647") ? "-" : time1);
 
         // Time
-        TextView textViewLevelTime1 = (TextView) findViewById(R.id.textViewLevelTime1);
-        textViewLevelTime1.setText(mPlayer.getGameTime().get(String.valueOf(AI.Level.I).toString()) + "s");
-        /*TextView textViewLevelTime2 = (TextView) findViewById(R.id.textViewLevelTime2);
-        textViewLevelTime2.setText(mPlayer.getGameTime().get(String.valueOf(AI.Level.II).toString()) + "s");
-        TextView textViewLevelTime3 = (TextView) findViewById(R.id.textViewLevelTime3);
-        textViewLevelTime3.setText(mPlayer.getGameTime().get(String.valueOf(AI.Level.III).toString()) + "s");
-        TextView textViewLevelTime4 = (TextView) findViewById(R.id.textViewLevelTime4);
-        textViewLevelTime4.setText(mPlayer.getGameTime().get(String.valueOf(AI.Level.IMPOSSIBLE).toString()) + "s");*/
+        TextView textViewLevelTime = (TextView) findViewById(R.id.textViewLevelTime1);
+        textViewLevelTime.setText(mPlayer.getGameTime().get(String.valueOf(levels[i]).toString()) + "s");
 
         // Total Time
         TextView textViewTotalTime = (TextView) findViewById(R.id.textViewLeveTotalTime);
         textViewTotalTime.setText(String.valueOf(mPlayer.getTotalGameTime()) + "s");
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
