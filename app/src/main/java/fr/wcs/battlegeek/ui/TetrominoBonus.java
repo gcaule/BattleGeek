18005acfdf573e3f647a7c7a60159f326efabc52
Adphi <philippe.adrien.nousse@gmail.com>
2017-10-16 16:06:29 +0200

    Add Bonus Symbol (need Work)

--- app/src/main/java/fr/wcs/battlegeek/ui/TetrominoBonus.java
+++ app/src/main/java/fr/wcs/battlegeek/ui/TetrominoBonus.java
@@ -1,6 +1,8 @@
 package fr.wcs.battlegeek.ui;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.PointF;
 
 import fr.wcs.battlegeek.model.Bonus;
@@ -14,6 +16,9 @@ public class TetrominoBonus extends TetrominoBlock {
     private final String TAG = Settings.TAG;
 
     private Bonus.Type mType;
+
+    // Graphics
+    private Paint mPaint = new Paint();
 
     public TetrominoBonus(PointF position, Bonus.Type type) {
         super(position, null);
@@ -36,10 +41,24 @@ public class TetrominoBonus extends TetrominoBlock {
             case CROSS_FIRE:
                 break;
         }
+
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        mPaint.setAntiAlias(true);
+        mPaint.setStrokeWidth(4);
     }
 
     @Override
     public void draw(Canvas canvas, float itemX, float itemY, float blockSize) {
         super.draw(canvas, itemX, itemY, blockSize);
+        float x = (itemX + mX) * mBlockSize;
+        float y = (itemY + mY) * mBlockSize;
+        float centerMargin = this.mBlockSize * 15 / 100;
+
+        float xPos = x + mBlockSize / 2 - (int)(mPaint.measureText(mType.toString())/2);
+        float yPos = (int) (y + mBlockSize / 2 - ((mPaint.descent() +mPaint.ascent()) / 2)) ;
+
+        mPaint.setTextSize(mBlockSize - centerMargin);
+        canvas.drawText(mType.toString(), xPos, yPos, mPaint);
     }
 }
