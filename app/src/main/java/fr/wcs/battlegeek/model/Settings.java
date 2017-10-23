29b92bcf5e822138369d1c77bde715334f9f2886
Adphi <philippe.adrien.nousse@gmail.com>
2017-10-23 11:49:37 +0200

    Changed Animations Predefined Speeds

--- app/src/main/java/fr/wcs/battlegeek/model/Settings.java
+++ app/src/main/java/fr/wcs/battlegeek/model/Settings.java
@@ -12,9 +12,9 @@ public class Settings {
 
     public static final String FILE_NAME = "Preferences";
     public static final int GRID_SIZE = 10;
-    public static final int ANIMATION_FAST = 600;
-    public static final int ANIMATION_MEDIUM = 1200;
-    public static final int ANIMATION_SLOW = 1800;
+    public static final int ANIMATION_FAST = 300;
+    public static final int ANIMATION_MEDIUM = 600;
+    public static final int ANIMATION_SLOW = 1200;
     public static final String ANIMATION_TAG = "AnimationsSpeed";
 
     public static final String MUSIC_TAG = "ValueMusic";
